b1dd0cb4ce6363c12e74b6597e65c3c3ee478649
rakusMikutoTabata <mikuto.tabata@rakus.co.jp>
2020-06-17 11:40:23 +0900

    AdministratorControllerのModel設定、Service宣言、inset.htmlへのフォワード処理追加

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,5 +1,35 @@
 package jp.co.sample.controller;
 
-public class AdministratorController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.service.AdministratorService;
+
+@Controller
+@RequestMapping("/")
+public class AdministratorController {
+	/**
+	 * SpringFrameworkから参照情報を注入
+	 */
+	@Autowired
+	private AdministratorService administratorservice;
+
+	/**
+	 * requestスコープにInsertAdministratorFormが自動格納
+	 */
+	@ModelAttribute
+	public InsertAdministratorForm setUpInsertAdministratorForm() {
+		return new InsertAdministratorForm();
+	}
+
+	/**
+	 * administrator/insert.htmlにフォワード
+	 */
+	@RequestMapping("/toInsert")
+	public String toInsert() {
+		return "administrator/insert";
+	}
 }
